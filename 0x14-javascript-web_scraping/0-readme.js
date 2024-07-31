c5d694361849437a2cee7efd7ac6e44f2c7c07d5
shehab <shehab.moh.shehab@gmail.com>
2024-07-31 03:28:26 +0300

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,19 +1,33 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 const path = require('path');
+
 function readFileContent(filename) {
+  return new Promise((resolve, reject) => {
     fs.readFile(filename, 'utf8', (err, data) => {
-        if (err) {
-            console.error(`Error reading file: ${err}`);
-            return;
-        }
-        console.log(`${data}`);
+      if (err) {
+        reject(`Error reading file: ${err}`);
+      } else {
+        resolve(data);
+      }
     });
+  });
 }
-if (process.argv.length < 3) {
-    console.error('Usage: node read.js <filename>');
-    process.exit(1);
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node read.js <filename>');
+  process.exit(1);
 }
+
 const filename = process.argv[2];
 const filePath = path.resolve(filename);
-readFileContent(filePath);
+
+readFileContent(filePath)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
